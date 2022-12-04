5ae4f39014e4fd56084f4f50cabfb32b333d589a
Daniel <dmforeacre@gmail.com>
2022-12-04 02:21:54 -0500

    Fixing gitignore pt2

--- target/debug/deps/AoC2022-1e33e980e0f57286.d
+++
@@ -1,6 +0,0 @@
-c:\Users\Daniel\OneDrive\MU\Code Challenges\AoC2022\target\debug\deps\AoC2022-1e33e980e0f57286.rmeta: src\main.rs src\day4\pt1.rs
-
-c:\Users\Daniel\OneDrive\MU\Code Challenges\AoC2022\target\debug\deps\AoC2022-1e33e980e0f57286.d: src\main.rs src\day4\pt1.rs
-
-src\main.rs:
-src\day4\pt1.rs:
