2484d99be69136e1d8701b09bafc27513e308367
Daniel <dmforeacre@gmail.com>
2022-12-18 21:10:43 -0500

    Added day 9

--- src/day9/pt1.rs
+++ src/day9/pt1.rs
@@ -2,6 +2,36 @@ use std::time::Instant;
 
 use crate::OutputStruct;
 
+fn updateTail(visited: &mut Vec<(i32,i32)>, count: i32, h: Vec<i32>, t: &mut Vec<i32>) -> i32 {
+    let diff = (h[0] - t[0], h[1] - t[1]);
+    //println!("H: {:?} T: {:?} diff: {:?}", h, t, diff);
+    if diff.0.abs() > 1 {
+        t[0] += diff.0.abs() / diff.0;
+        if h[1] > t[1] {
+            t[1] += 1;
+        }
+        else if h[1] < t[1] {
+            t[1] -= 1;
+        }
+    }
+    if diff.1.abs() > 1 {
+        t[1] += diff.1.abs() / diff.1;
+        if h[0] > t[0] {
+            t[0] += 1;
+        }
+        else if h[0] < t[0] {
+            t[0] -= 1;
+        }
+    }
+
+    if visited.iter().find(|&&x| x == (t[0],t[1])) == None {
+        visited.push((t[0],t[1]));
+        return count + 1;
+    }
+
+    return count;
+}
+
 pub fn pt1() -> OutputStruct::OutputStruct {
     let mut output = OutputStruct::new();
     let mut clock = Instant::now();
@@ -11,7 +41,51 @@ pub fn pt1() -> OutputStruct::OutputStruct {
     output.parseTime = clock.elapsed().as_micros();
     clock = Instant::now();
 
+    let mut headPos = vec![0;2];
+    let mut tailPos = vec![0;2];
+
+    let mut visited: Vec<(i32,i32)> = Vec::new();
+    let mut count = 0;
+
+    for line in string.trim().split("\n") {
+        let tokens: Vec<&str> = line.trim().split(" ").collect();
+        match tokens[0] {
+            "U" => {
+                for _ in 0..tokens[1].parse::<i32>().unwrap() {
+                    headPos[1] += 1;
+                    count = updateTail(&mut visited, count, headPos.clone(), &mut tailPos);
+                }
+                //println!("Up");
+            },
+            "D" => {
+                for _ in 0..tokens[1].parse::<i32>().unwrap() {
+                    headPos[1] -= 1;
+                    count = updateTail(&mut visited, count, headPos.clone(), &mut tailPos);
+                }
+                //println!("Down");
+            },
+            "L" => {
+                for _ in 0..tokens[1].parse::<i32>().unwrap() {
+                    headPos[0] -= 1;
+                    count = updateTail(&mut visited, count, headPos.clone(), &mut tailPos);
+                }
+                //println!("Left");
+            },
+            "R" => {
+                for _ in 0..tokens[1].parse::<i32>().unwrap() {
+                    headPos[0] += 1;
+                    count = updateTail(&mut visited, count, headPos.clone(), &mut tailPos);
+                }
+                //println!("Right");
+            },
+            _ => break,
+        }
+    }
+
+    //println!("{:?} {:?}", headPos, tailPos);
+
     output.calcTime = clock.elapsed().as_micros();
+    output.answer = count.to_string();
 
     return output;
 }
--- src/day9/pt2.rs
+++ src/day9/pt2.rs
@@ -2,6 +2,22 @@ use std::time::Instant;
 
 use crate::OutputStruct;
 
+fn updateTail(h: Vec<i32>, t: &mut Vec<i32>) {
+    let diff = (h[0] - t[0], h[1] - t[1]);
+    if diff.0.abs() > 1 {
+        t[0] += diff.0.abs() / diff.0;
+        if diff.1.abs() == 1 {
+            t[1] += diff.1.abs() / diff.1;
+        }
+    }
+    if diff.1.abs() > 1 {
+        t[1] += diff.1.abs() / diff.1;
+        if diff.0.abs() == 1 {
+            t[0] += diff.0.abs() / diff.0;
+        }
+    }
+}
+
 pub fn pt2() -> OutputStruct::OutputStruct {
     let mut output = OutputStruct::new();
     let mut clock = Instant::now();
@@ -11,7 +27,67 @@ pub fn pt2() -> OutputStruct::OutputStruct {
     output.parseTime = clock.elapsed().as_micros();
     clock = Instant::now();
 
+    let mut knots = vec![vec![0;2];10];
+
+    let mut visited: Vec<(i32,i32)> = Vec::new();
+    let mut count = 0;
+
+    for line in string.trim().split("\n") {
+        let tokens: Vec<&str> = line.trim().split(" ").collect();
+        match tokens[0] {
+            "U" => {
+                for _ in 0..tokens[1].parse::<i32>().unwrap() {
+                    knots[0][1] += 1;
+                    for i in 0..9 {
+                        updateTail(knots[i].clone(), &mut knots[i + 1]);
+                    }
+                    if visited.iter().find(|&&x| x == (knots[9][0],knots[9][1])) == None {
+                        visited.push((knots[9][0],knots[9][1]));
+                        count += 1;
+                    }
+                }
+            },
+            "D" => {
+                for _ in 0..tokens[1].parse::<i32>().unwrap() {
+                    knots[0][1] -= 1;
+                    for i in 0..9 {
+                        updateTail(knots[i].clone(), &mut knots[i + 1]);
+                    }
+                    if visited.iter().find(|&&x| x == (knots[9][0],knots[9][1])) == None {
+                        visited.push((knots[9][0],knots[9][1]));
+                        count += 1;
+                    }
+                }
+            },
+            "L" => {
+                for _ in 0..tokens[1].parse::<i32>().unwrap() {
+                    knots[0][0] -= 1;
+                    for i in 0..9 {
+                        updateTail(knots[i].clone(), &mut knots[i + 1]);
+                    }
+                    if visited.iter().find(|&&x| x == (knots[9][0],knots[9][1])) == None {
+                        visited.push((knots[9][0],knots[9][1]));
+                        count += 1;
+                    }
+                }
+            },
+            "R" => {
+                for _ in 0..tokens[1].parse::<i32>().unwrap() {
+                    knots[0][0] += 1;
+                    for i in 0..9 {
+                        updateTail(knots[i].clone(), &mut knots[i + 1]);
+                    }
+                    if visited.iter().find(|&&x| x == (knots[9][0],knots[9][1])) == None {
+                        visited.push((knots[9][0],knots[9][1]));
+                        count += 1;
+                    }
+                }
+            },
+            _ => break,
+        }
+    }
     output.calcTime = clock.elapsed().as_micros();
+    output.answer = count.to_string();
 
     return output;
 }
